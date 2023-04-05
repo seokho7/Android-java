b8203081b145254b436090d0a6e0a33789f56254
seokho7 <rkwhr3256@naver.com>
2023-04-05 11:30:18 +0900

    테스트 커밋

--- app/src/main/java/com/example/exampleboxjava/MainActivity.java
+++ app/src/main/java/com/example/exampleboxjava/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("커밋 테스트");
+        System.out.println("커밋 2차 테스트 왜 안돼 이거");
     }
 }
